3c88145fa0b972c06700b0646e20ab85012d9402
RenetRamman <renet.ramman@gmail.com>
2026-02-21 22:11:35 +0200

    moved to firebase, added debug grid, circular brush

---
+++ public/js/index.js
@@ -0,0 +1,282 @@
+var renderer,
+  uniforms,
+  vshader,
+  fshader,
+  camera,
+  scene,
+  bufferScene,
+  mesh,
+  deltaTime,
+  timeSinceStart,
+  zoom,
+  brushSize;
+
+var paused = false;
+var nextFrameRequested = false;
+var frameReady = true;
+var framerate = 61;
+var lastFrameTime = 0;
+var images = Array();
+var shaders = {
+  vertexShader: "shaders/vertex.vert",
+  fragmentShader: "shaders/game_of_life.frag",
+  bufferShader: "shaders/buffer.frag",
+  image: "christmas.jpg", // TODO: add option to easily change input image
+};
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+var loader = new THREE.FileLoader();
+var texLoader = new THREE.TextureLoader();
+texLoader.setPath("/images/");
+
+// Hamburger menu stuff
+const menuButton = document.getElementById("menuButton");
+const menuPanel = document.getElementById("menuPanel");
+const pauseText = document.getElementById("pauseAdditional");
+const framerateText = document.getElementById("fpsText");
+
+menuButton.addEventListener("click", () => {
+  menuPanel.style.display =
+    menuPanel.style.display === "block" ? "none" : "block";
+});
+document.addEventListener("click", (e) => {
+  if (!menu.contains(e.target)) {
+    menuPanel.style.display = "none";
+  }
+});
+window.addEventListener("keydown", (e) => {
+  if (e.key === "n") nextFrameRequested = true;
+});
+
+// Control hooks
+const zoomSlider = document.getElementById("zoomSlider");
+const brushSlider = document.getElementById("brushSlider");
+const speedSlider = document.getElementById("speedSlider");
+const pauseToggle = document.getElementById("pauseToggle");
+const gridToggle = document.getElementById("gridToggle");
+const imageSelect = document.getElementById("imageSelect");
+
+async function init() {
+  var files_loaded = 0;
+
+  function continueIfReady(files, count) {
+    count += 1;
+    if (count === Object.keys(files).length) {
+      finishShaderLoading();
+    }
+    return count;
+  }
+
+  // TODO: Refac this mess. Add loader or file type to dict
+  for (let [shader, value] of Object.entries(shaders)) {
+    if (shader != "image") {
+      await loader.load(value, function (data) {
+        shaders[shader] = data;
+        files_loaded = continueIfReady(shaders, files_loaded);
+      });
+    } else {
+      await texLoader.load(value, function (data) {
+        shaders[shader] = data;
+        files_loaded = continueIfReady(shaders, files_loaded);
+      });
+    }
+  }
+}
+
+init();
+
+function finishShaderLoading() {
+  timeSinceStart = 0.0;
+  zoom = 1.0;
+  const canvas = document.getElementById("c");
+  renderer = new THREE.WebGLRenderer({ canvas });
+
+  scene = new THREE.Scene();
+  bufferScene = new THREE.Scene();
+
+  const geometry = new THREE.PlaneGeometry(2, 2);
+
+  const resolution = new THREE.Vector3(
+    sizes.width,
+    sizes.height,
+    window.devicePixelRatio,
+  );
+
+  const imageResolution = new THREE.Vector2(
+    shaders.image.image.width,
+    shaders.image.image.height,
+  );
+
+  var renderBufferA = new THREE.WebGLRenderTarget(sizes.width, sizes.height, {
+    minFilter: THREE.NearestFilter,
+    magFilter: THREE.NearestFilter,
+    format: THREE.RGBAFormat,
+    type: THREE.FloatType,
+    stencilBuffer: false,
+  });
+
+  var renderBufferB = new THREE.WebGLRenderTarget(sizes.width, sizes.height, {
+    minFilter: THREE.NearestFilter,
+    magFilter: THREE.NearestFilter,
+    format: THREE.RGBAFormat,
+    type: THREE.FloatType,
+    stencilBuffer: false,
+  });
+
+  const uniforms = {
+    u_texture: {
+      value: shaders.image,
+    },
+    u_resolution: { value: resolution },
+    u_image_resolution: { value: imageResolution },
+    u_time: { value: 0.0 },
+    u_mouse: { value: { x: 0, y: 0 } },
+    u_zoom: { value: 1.0 },
+    u_brush_size: { value: 1.0 },
+    u_grid_enable: { value: false },
+  };
+
+  const bufferMaterial = new THREE.ShaderMaterial({
+    uniforms: uniforms,
+    vertexShader: shaders.vertexShader,
+    fragmentShader: shaders.bufferShader,
+  });
+
+  const material = new THREE.ShaderMaterial({
+    uniforms: uniforms,
+    vertexShader: shaders.vertexShader,
+    fragmentShader: shaders.fragmentShader,
+  });
+
+  const mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
+
+  const bufferMesh = new THREE.Mesh(geometry, bufferMaterial);
+  bufferScene.add(bufferMesh);
+
+  camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0.1, 10);
+  camera.position.z = 1;
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  const clock = new THREE.Clock();
+
+  onWindowResize();
+  if ("ontouchstart" in window) {
+    document.addEventListener("touchmove", move);
+  } else {
+    window.addEventListener("resize", onWindowResize, false);
+    document.addEventListener("mousemove", move);
+    document.addEventListener("wheel", doZoom);
+  }
+
+  // FIXME: inaccurate scroll wheel zoom due to floating point errors.
+  // Use integers divided by 10 instead?
+  function doZoom(evt) {
+    if (evt.deltaY < 0 && zoom >= 0.2) {
+      zoom -= 0.1;
+    } else if (evt.deltaY > 0 && zoom < 1) {
+      zoom += 0.1;
+    }
+    material.uniforms.u_zoom.value = zoom;
+    bufferMaterial.uniforms.u_zoom.value = zoom;
+  }
+
+  function move(evt) {
+    material.uniforms.u_mouse.value.x = evt.touches
+      ? evt.touches[0].clientX
+      : evt.clientX;
+    material.uniforms.u_mouse.value.y = evt.touches
+      ? evt.touches[0].clientY
+      : resolution.y - evt.clientY;
+    bufferMaterial.uniforms.u_mouse.value.x = evt.touches
+      ? evt.touches[0].clientX
+      : evt.clientX;
+    bufferMaterial.uniforms.u_mouse.value.y = evt.touches
+      ? evt.touches[0].clientY
+      : resolution.y - evt.clientY;
+    return;
+  }
+
+  zoomSlider.addEventListener("input", (e) => {
+    zoom = parseFloat(e.target.value);
+    uniforms.u_zoom.value = zoom;
+  });
+
+  brushSlider.addEventListener("input", (e) => {
+    uniforms.u_brush_size.value = parseFloat(e.target.value);
+  });
+
+  speedSlider.addEventListener("input", (e) => {
+    framerate = parseFloat(e.target.value);
+    framerateText.textContent = framerate != 61 ? framerate : "INF";
+  });
+
+  pauseToggle.addEventListener("change", (e) => {
+    paused = e.target.checked;
+    pauseText.style.display =
+      pauseText.style.display === "block" ? "none" : "block";
+  });
+
+  gridToggle.addEventListener("change", (e) => {
+    uniforms.u_grid_enable.value = e.target.checked;
+  });
+
+  imageSelect.addEventListener("change", (e) => {
+    const selectedImage = e.target.value;
+    texLoader.load(selectedImage, function (data) {
+      data.minFilter = THREE.NearestFilter;
+      data.magFilter = THREE.NearestFilter;
+      frameReady = false;
+      uniforms.u_texture.value = data;
+      uniforms.u_time.value = 0.0;
+      timeSinceStart = 0;
+    });
+  });
+
+  animate();
+
+  // FIXME: This messes up the simulation
+  function onWindowResize(event) {
+    console.log(renderer.context.drawingBufferWidth);
+    console.log(window.innerWidth);
+    sizes.width = window.innerWidth;
+    sizes.height = window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+    renderer.setSize(sizes.width, sizes.height);
+    uniforms.u_resolution.value.x = sizes.width;
+    uniforms.u_resolution.value.y = sizes.height;
+  }
+
+  function animate() {
+    zoomSlider.value = zoom;
+    frameReady = framerate === 61 ? true : 1 / framerate <= lastFrameTime;
+    deltaTime = clock.getDelta();
+    if ((!paused && frameReady) || nextFrameRequested) {
+      renderer.setRenderTarget(renderBufferA);
+      renderer.render(bufferScene, camera);
+      mesh.material.uniforms.u_texture.value = renderBufferA.texture;
+
+      // ping-pong buffering
+      const temp = renderBufferA;
+      renderBufferA = renderBufferB;
+      renderBufferB = temp;
+      bufferMaterial.uniforms.u_texture.value = renderBufferB.texture;
+      nextFrameRequested = false;
+      lastFrameTime = 0;
+    }
+    lastFrameTime += deltaTime;
+
+    renderer.setRenderTarget(null);
+    renderer.render(scene, camera);
+
+    requestAnimationFrame(animate);
+    timeSinceStart += deltaTime;
+    material.uniforms.u_time.value = timeSinceStart;
+    bufferMaterial.uniforms.u_time.value = timeSinceStart;
+    // console.log(mesh.material.uniforms.u_time.value);
+    // console.log(mesh.material.uniforms.u_mouse.value);
+    // console.log(mesh.material.uniforms.u_resolution);
+  }
+}
---
+++ public/shaders/buffer.frag
@@ -0,0 +1,92 @@
+precision mediump float;
+uniform sampler2D u_texture;
+uniform vec2 u_resolution;
+uniform float u_time;
+uniform vec2 u_mouse;
+uniform float u_zoom;
+uniform float u_brush_size;
+uniform bool u_grid_enable;
+
+varying vec2 v_uv;
+
+float GetNeighbours(vec2 p) {
+    float count = 0.0;
+
+    for (float y = -1.0; y <= 1.0; y++) {
+        for (float x = -1.0; x <= 1.0; x++) {
+            if (x == 0.0 && y == 0.0)
+                continue;
+
+            // Scale the offset down
+            vec2 offset = vec2(x, y) / u_resolution.xy;
+            // Apply offset and sample texture
+            vec4 lookup = texture2D(u_texture, p + offset);
+            // Accumulate the result
+            count += lookup.r > 0.5 ? 1.0 : 0.0;
+        }
+    }
+
+    return count;
+}
+
+
+
+void main() {
+    vec3 color = vec3(0.0);
+    float neighbors = GetNeighbours(v_uv);
+    bool alive = texture2D(u_texture, v_uv).r > 0.5;
+    float delay = 1.0;
+
+    // is cell alive
+    if (neighbors == 3.0 || (alive && neighbors == 2.0)) {
+        color = vec3(1.0);
+    }
+
+    // // show image for some time
+    // if (u_time <= delay) {
+    //     color = texture2D(u_texture, v_uv).rgb;
+    // }
+
+    // color = vec3(v_uv.x, v_uv.y, 1.0);
+    // color = vec3(gl_FragCoord.xy / u_resolution.xy, 1.0);
+
+    // // Debug lines for pixel width
+    // float x = gl_FragCoord.x;
+    // // Alternate every 1 pixel
+    // float stripe = mod(floor(x), 2.0);
+    // color = vec3(stripe);
+
+    // // Debug grid
+    // float x = gl_FragCoord.x;
+    // float stripex = mod(floor(x), 2.0);
+    // float y = gl_FragCoord.y;
+    // float stripey = mod(floor(y), 2.0);
+    // color += vec3(stripex * 0.5, 0.0, 0.0);
+    // color += vec3(0.0, stripey * 0.5, 0.0);
+
+    // Debug grid gradient
+    if (u_grid_enable) {
+        float x = gl_FragCoord.x;
+        float stripex = mod(floor(x), 2.0);
+        float y = gl_FragCoord.y;
+        float stripey = mod(floor(y), 2.0);
+        color += vec3(stripex * 0.5 * (x / u_resolution.x), 0.0, 0.0);
+        color += vec3(0.0, stripey * 0.5 * (y / u_resolution.y), 0.0);
+    }
+
+
+    // // Debug cross
+    // if (distance(u_mouse.x, gl_FragCoord.x) < 10.0) {
+    //     color = vec3(0.0);
+    // }
+    // if (distance(u_mouse.y, gl_FragCoord.y ) < 10.0) {
+    //     color = vec3(0.0);
+    // }
+
+    // Draw on screen
+    if (distance(u_mouse.xy, gl_FragCoord.xy) < 1.0 * u_brush_size && u_time > delay) {
+        color = vec3(1.0);
+    }
+
+    gl_FragColor = vec4(color, 1.0);
+}
